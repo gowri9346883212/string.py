c99b4dbd53def1ec32dc3106aeae6f7635b15340
gowri9346883212 <145899033+gowri9346883212@users.noreply.github.com>
2023-09-25 15:11:27 +0530

    Add files via upload

---
+++ string.py.py
@@ -0,0 +1,111 @@
+#string datatype
+#implicit
+name = "gowri"
+print(type(name))
+
+#explicit/type convertion
+name = str("gowri")
+print(type(name))
+
+#data type annotation
+name: str = "gowri"
+print(type(name))
+
+#string type conversion
+#integer to string
+#implicit
+num = 10
+print(num)
+print(type(num))
+
+#explicit
+num = str(10)
+print(num)
+print(type(num))
+
+#data type annotation
+num: str = 10
+print(num)
+print(type(num))
+
+#float to string
+#implicit
+num = 10
+print(num)
+print(type(num))
+
+#explicit
+num = str(10)
+print(num)
+print(type(num))
+
+#data tyte annotation
+num: str = 10.10
+print(num)
+print(type(num))
+
+#binary to string
+#implicit
+a = 10
+print(a)
+print(type(a))
+
+#explicit
+a = str(10)
+print(a)
+print(type(a))
+
+#datatype annotation
+a: str=10
+print(a)
+print(type(a))
+
+#hexadecimal to strig
+#implicit
+a=0xface
+print(a)
+print(type(a))
+
+#explicit
+a= str(0x12ab)
+print(a)
+print(type(a))
+
+#data annonation
+a: str=0x1234
+print(a)
+print(type(a))
+
+#oct to str
+#implicit
+a=0o1234
+print(a)
+print(type(a))
+
+#explicit
+a=str(0o1234)
+print(a)
+print(type(a))
+
+#data annonation
+a: str=0o1234
+print(a)
+print(type(a))
+
+#binary to str
+#implicit
+a=0b1010
+print(a)
+print(type(a))
+
+#explicit/type convertion
+a=str(0b1010)
+print(a)
+print(type(a))
+
+#data type annotation
+a: str=0b1010
+print(a)
+print(type(a))
+
+
