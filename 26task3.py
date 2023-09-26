dbfb8858dbd2137af6d8cf0bd8d4afe03f9bf910
gowri9346883212 <145899033+gowri9346883212@users.noreply.github.com>
2023-09-26 16:48:29 +0530

    Add files via upload

---
+++ 26task3.py
@@ -0,0 +1,89 @@
+#string tranform funcctions
+#capitalize
+data = "hi how are you?"
+print(data.capitalize())
+
+#title
+name = " gowri "
+print(name.title())
+
+#upper
+name = " gowri "
+print(name.upper())
+
+#lower
+name = " gowri "
+print(name.lower())
+
+#casefold
+name = " gowri "
+print(name.casefold())
+
+#swap case
+name = " gowri "
+print(name.swapcase())
+
+#string check functions
+#isnumeric
+name = " 123 "
+print(name.isnumeric())
+
+#isalnum
+a = "gowri415"
+print(a.isalnum())
+a = "12.2"
+print(a.isalnum())
+
+#isdecimal
+a = "gowri415"
+print(a.isdecimal())
+a = "415"
+print(a.isdecimal())
+
+#isdigit
+a = "415"
+print(a.isdecimal())
+a = " gowri "
+print(a.isdecimal())
+
+#isascii
+a = "415"
+print(a.isascii())
+a = " GOWRI "
+print(a.isascii())
+
+#isupper
+name = " gowri "
+print(name.isupper())
+name = " GOWRI "
+print(name.isupper())
+
+#islower
+name = " GOWRI "
+print(name.islower())
+name = "gowri"
+print(name.islower())
+
+#isspace
+name = " gowri  "
+print(name.isspace())
+name = "   "
+print(name.isspace())
+
+#isidentifies
+name= "gowri"
+print(name.isidentifier())
+name= "231"
+print(name.isidentifier())
+
+#isprintable
+name = " gowri "
+print(name.isprintable())
+
+#istitle
+name = " gowri "
+print(name.istitle())
+name = " Gowri "
+print(name.istitle())
+
+
