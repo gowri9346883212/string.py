d97b48671fe481ee7833b388d5d3080a26c9ea16
gowri9346883212 <145899033+gowri9346883212@users.noreply.github.com>
2023-09-29 18:50:37 +0530

    Add files via upload

---
+++ 29task4.py
@@ -0,0 +1,136 @@
+#operators
+#arthematic operators
+#addition
+a = 5
+b = 5
+print(a + b)
+
+#substraction
+a = 10
+b = 4
+print(a - b)
+
+#multipication
+a = 3
+b = 3
+print(a * b)
+
+#division
+a = 10
+b = 2
+print(a / b)
+
+#module
+a = 8
+b = 2
+print(a % b)
+
+#floor division
+a = 4
+b = 6
+print(a // b)
+
+#assignment operators
+#equal to
+a = b =10
+print(a)
+print(b)
+
+#addition equal to
+a = 2
+a+= 2
+print(a)
+
+#substraction equal to
+a = 8
+a-= 4
+print(a)
+
+#multipication equal to
+a = 5
+a*= 2
+print(a)
+
+#module equal to
+a = 9
+a%= 3
+print(a)
+#division equal to
+a = 9
+a/= 2
+print(a)
+
+#floordivision equal to
+a = 8
+a//= 2
+print(a)
+
+#logical operators
+#and operator
+a = 5
+b = 10
+c = 7
+print(a < b and a < c)
+
+#or operator
+a = 5
+b = 10
+c = 7
+print(a < b or a > c)
+
+#not operator
+a = 5
+b = 10
+c = 7
+print(a < b or a < c)
+
+#comparison operators
+#double equal to
+a = 5
+b = 5
+print(a == b)
+
+#lessthan equal to
+a = 5
+b = 10
+print(a <= b)
+
+#getherthan equal to
+a = 5
+b = 4
+print(a >= b)
+
+#! equal to
+a = 5
+b = 10
+print(a != b)
+
+#lessthan
+a = 5
+b = 10
+print(a < b)
+
+#getherthan
+a = 5
+b = 4
+print(a > b)
+
+#bitwise operator
+#and operator
+a = 6
+b = 2
+print(a & b)
+
+#or operator
+a = 6
+b = 2
+print(a | b)
+
+#xor operator
+a = 6
+b = 2
+print(a ^ b)
+
+
+
+
