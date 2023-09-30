c2572fc4fccbc8364ff16a41005c8ea1f5859437
gowri9346883212 <145899033+gowri9346883212@users.noreply.github.com>
2023-09-30 17:41:40 +0530

    Add files via upload

---
+++ 30task5.py
@@ -0,0 +1,40 @@
+#left shift operator
+print(8>>2)
+
+#right shift operator
+print(1<<2)
+
+#check address of a variable: id:
+a = 10
+print(id(a))
+
+#identity operator
+#is
+a = 10
+b = 10
+print(a is b)
+
+#is not
+a = 10
+b = 10
+print(a is not b)
+
+#membership operator
+#in operator
+a = "gowri@gmail.com"
+print("@" in a)
+attendees = ["gowri", "satya", "swarna"]
+name = "gowri"
+print(name in attendees)
+
+#not in
+a = "gowri@gmail.com"
+print("@" in a)
+attendees = ["gowri", "satya", "swarna"]
+name = "ganga"
+print(name in attendees)
+
+
+
+
+
